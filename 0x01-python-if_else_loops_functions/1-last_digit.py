ff4cdcb69018caccf280a4e2cc45781ca7c0ff43
Michael Achumba <michaelachumba@gmail.com>
2024-03-17 15:48:15 -0700

    Corrects error in 1-last_digit code again

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,6 @@ ld = abs(number) % 10
 if number < 0:
     ld = -ld
 print(f"Last digit of {number} is {ld} and is ", end="")
-if ld < 0:
-    ld = -ld
 if ld > 5:
     print("greater than 5")
 elif ld > 0 and ld < 6:
