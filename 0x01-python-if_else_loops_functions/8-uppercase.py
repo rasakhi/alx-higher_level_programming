d5cffefecf5e856a91f68e5831921c3a45c9e9b7
Michael Achumba <michaelachumba@gmail.com>
2024-03-20 00:39:42 -0700

    Pythno function that prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    ustr = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            ustr = ustr + chr(ord(i) - 32)
+        else:
+            ustr = ustr + i
+    print(ustr)
