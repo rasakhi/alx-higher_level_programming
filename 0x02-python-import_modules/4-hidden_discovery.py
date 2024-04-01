2f3a3ce7ffadfccd5b0930e3b97ebf16864dc510
Michael Achumba <michaelachumba@gmail.com>
2024-04-01 05:51:58 -0700

    Fixed requirement error - task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
-for i in dir(hidden_4):
-    if i.startswith('__'):
-        pass
-    else:
-        print("{}".format(i))
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i.startswith('__'):
+            pass
+        else:
+            print("{}".format(i))
