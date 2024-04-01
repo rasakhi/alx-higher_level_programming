228ebe427bb2cf3d25e83d2f0e023a13b2dbb5cb
Michael Achumba <michaelachumba@gmail.com>
2024-04-01 16:52:02 -0700

    Solves task 6 - take 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,14 +5,13 @@ if __name__ == "__main__":
     import sys
 
     argv_len = len(sys.argv)
-    if argv_len == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        operators = {"+": add, "-": sub, "*": mul, "/": div}
-
-        if sys.argv[2] not in list(operators.keys()):
-            sys.exit("Unknown operator. Available operators: +, -, * and /")
-        else:
-            print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
-    else:
+    if argv_len != 4:
         sys.exit("Usage: ./100-my_calculator.py <a> <operator> <b>")
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        sys.exit("Unknown operator. Available operators: +, -, * and /")
+        
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b))
